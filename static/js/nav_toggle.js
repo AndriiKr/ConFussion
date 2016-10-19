bd7352ff5597ebc9b4a793f849cbb8415a98611d
AndriiKr <1drun1@gmail.com>
2016-10-19 16:44:05 +0300

    last one

--- static/js/nav_toggle.js
+++ static/js/nav_toggle.js
@@ -1,20 +1,5 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("navbar").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.navbar-toggle')) {
-
-    var dropdowns = document.getElementsByClassName("navbar-collapse");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
