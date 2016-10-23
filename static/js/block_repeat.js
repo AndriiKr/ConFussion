eed443f568658eae98981c4fcec7365e925783e2
AndriiKr <1drun1@gmail.com>
2016-10-23 17:48:50 +0300

    blueprint done

---
+++ static/js/block_repeat.js
@@ -0,0 +1,10 @@
+
+// повторяем блок .box 12 раз со всеми вложениями
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.box'), 12, true);
